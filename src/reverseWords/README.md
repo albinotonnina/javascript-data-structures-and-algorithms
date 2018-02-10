7bf1bba49a97d9bbd3a12be6c483fa3d2b5bef37
Albino Tonnina <albinotonnina@gmail.com>
2018-02-10 17:53:23 +0000

    🎉 reverse words

---
+++ src/reverseWords/README.md
@@ -0,0 +1,7 @@
+reverseWords:
+
+Reverse the letters in every word ut don't use Array.reverse()
+
+```
+ expect(reverseWords('I like JavaScript problems')).toEqual('I ekil tpircSavaJ smelborp')
+```
---
+++ src/reverseWords/__tests__/reverseWords.test.js
@@ -0,0 +1,7 @@
+import reverseWords from '../'
+
+describe('reverseWords', () => {
+  it('should behave...', () => {
+    expect(reverseWords('I like JavaScript problems')).toEqual('I ekil tpircSavaJ smelborp')
+  })
+})
