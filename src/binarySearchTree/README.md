319dedac51052d68abe08f83e46df19805b828a2
Albino Tonnina <albinotonnina@gmail.com>
2018-02-11 21:14:43 +0000

    🛠🚀 binary search tree

---
+++ src/binarySearchTree/README.md
@@ -0,0 +1,32 @@
+
+# ⚗️ Binary search tree implementation
+
+ES6 and classes
+
+![alt text](https://img.ziggi.org/pRzheJmf.gif "Binary search tree")
+
+## Complexity
+* delete: Linear — O(n), or O(log n) in average case
+* insert: Linear — O(n), or O(log n) in average case
+* contains: Linear — O(n), or O(log n) in average case
+* depthFirstLog: Linear — O(n), or O(log n) in average case
+
+### Install
+`npm install`
+
+### Test
+`npm run test`
+
+### Start
+`npm start`
+
+
+## Exercises
+
+* Design an algorithm to find a path from one node in a binary tree to another.
+
+* Given a binary tree, check whether it’s a binary search tree or not.
+
+* Find the minimum depth of binary search tree
+
+* Given a binary search tree and a value k, please find a node in the binary search tree whose value is closest to k.
---
+++ src/binarySearchTree/index.js
@@ -0,0 +1,72 @@
+class BinarySearchTree {
+  constructor(value) {
+    this.value = value
+    this.left = null
+    this.right = null
+  }
+
+  insert(value) {
+    const node = new BinarySearchTree(value)
+
+    const recurse = bst => {
+      if (!bst.value && bst.value !== 0) {
+        bst.value = node.value
+      } else if (bst.value > value && bst.left === null) {
+        bst.left = node
+      } else if (bst.value > value) {
+        recurse(bst.left)
+      } else if (bst.value < value && bst.right === null) {
+        bst.right = node
+      } else if (bst.value < value) {
+        recurse(bst.right)
+      }
+    }
+
+    recurse(this)
+  }
+
+  contains(value) {
+    let doesContain = false
+
+    const recurse = bst => {
+      if (bst.value === value) {
+        doesContain = true
+      } else if (bst.left !== null && value < bst.value) {
+        recurse(bst.left)
+      } else if (bst.right !== null && value > bst.value) {
+        recurse(bst.right)
+      }
+    }
+
+    recurse(this)
+    return doesContain
+  }
+
+  depthFirstLog(callback, order = 'pre-order') {
+    const recurse = bst => {
+      if (order === 'pre-order') {
+        callback.call(bst, bst.value)
+      }
+
+      if (bst.left !== null) {
+        recurse(bst.left)
+      }
+
+      if (order === 'in-order') {
+        callback.call(bst, bst.value)
+      }
+
+      if (bst.right !== null) {
+        recurse(bst.right)
+      }
+
+      if (order === 'post-order') {
+        callback.call(bst, bst.value)
+      }
+    }
+
+    recurse(this)
+  }
+}
+
+module.exports = BinarySearchTree
