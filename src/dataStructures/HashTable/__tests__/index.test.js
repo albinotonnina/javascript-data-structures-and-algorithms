07265d05829fb859537a2b3e922af7e80f32055a
Albino Tonnina <albinotonnina@gmail.com>
2018-02-12 00:14:20 +0000

    🛠 change implementation to bucketing hash map

--- src/dataStructures/HashTable/__tests__/index.test.js
+++ src/dataStructures/HashTable/__tests__/index.test.js
@@ -2,19 +2,100 @@ import HashTable from '../'
 
 describe('HashTable', () => {
   it('should pass every test', () => {
-    var hashTable = new HashTable(3)
-    hashTable.add('first', 1)
-    hashTable.add('second', 2)
-    hashTable.add('third', 3)
-    hashTable.add('fourth', 4)
-    hashTable.add('fifth', 5)
-    expect(hashTable.print()).toEqual('2 4 1 3 5')
-    expect(hashTable.length()).toEqual(5)
-    expect(hashTable.search('second')).toEqual(2)
+    var hashT = new HashTable()
 
-    hashTable.remove('fourth')
-    hashTable.remove('first')
-    expect(hashTable.print()).toEqual('2 3 5')
-    expect(hashTable.length()).toEqual(3)
+    hashT.insert('Alex Hawkins', '510-599-1930')
+
+    expect(hashT.retrieve('Alex Hawkins')).toEqual('510-599-1930')
+
+    hashT.insert('Boo Radley', '520-589-1970')
+
+    expect(hashT.retrieveAll()).toEqual([
+      undefined,
+      [['Boo Radley', '520-589-1970']],
+      undefined,
+      [['Alex Hawkins', '510-599-1930']]
+    ])
+
+    hashT
+      .insert('Vance Carter', '120-589-1970')
+      .insert('Rick Mires', '520-589-1970')
+      .insert('Tom Bradey', '520-589-1970')
+      .insert('Biff Tanin', '520-589-1970')
+
+    expect(hashT.retrieveAll()).toEqual([
+      undefined,
+      [['Boo Radley', '520-589-1970'], ['Tom Bradey', '520-589-1970']],
+      [['Vance Carter', '120-589-1970']],
+      [['Alex Hawkins', '510-599-1930'], ['Rick Mires', '520-589-1970']],
+      undefined,
+      undefined,
+      [['Biff Tanin', '520-589-1970']]
+    ])
+
+    //overide example (Phone Number Change)
+    //
+    hashT
+      .insert('Rick Mires', '650-589-1970')
+      .insert('Tom Bradey', '818-589-1970')
+      .insert('Biff Tanin', '987-589-1970')
+
+    expect(hashT.retrieveAll()).toEqual([
+      undefined,
+      [['Boo Radley', '520-589-1970'], ['Tom Bradey', '818-589-1970']],
+      [['Vance Carter', '120-589-1970']],
+      [['Alex Hawkins', '510-599-1930'], ['Rick Mires', '650-589-1970']],
+      undefined,
+      undefined,
+      [['Biff Tanin', '987-589-1970']]
+    ])
+
+    hashT.remove('Rick Mires')
+    hashT.remove('Tom Bradey')
+
+    expect(hashT.retrieveAll()).toEqual([
+      undefined,
+      [['Boo Radley', '520-589-1970']],
+      [['Vance Carter', '120-589-1970']],
+      [['Alex Hawkins', '510-599-1930']],
+      undefined,
+      undefined,
+      [['Biff Tanin', '987-589-1970']]
+    ])
+
+    hashT
+      .insert('Dick Mires', '650-589-1970')
+      .insert('Lam James', '818-589-1970')
+      .insert('Ricky Ticky Tavi', '987-589-1970')
+
+    expect(hashT.retrieveAll()).toEqual([
+      undefined,
+      undefined,
+      [['Vance Carter', '120-589-1970']],
+      [
+        ['Alex Hawkins', '510-599-1930'],
+        ['Dick Mires', '650-589-1970'],
+        ['Lam James', '818-589-1970']
+      ],
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      [['Boo Radley', '520-589-1970'], ['Ricky Ticky Tavi', '987-589-1970']],
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      [['Biff Tanin', '987-589-1970']]
+    ])
+
+    /* NOTICE HOW HASH TABLE HAS NOW DOUBLED IN SIZE UPON REACHING 75% CAPACITY ie 6/8. It is now size 16.
+    */
+
+    expect(hashT.retrieve('Lam James')).toEqual('818-589-1970') //818-589-1970
+    expect(hashT.retrieve('Dick Mires')).toEqual('650-589-1970') //818-589-1970
+
+    expect(hashT.retrieve('Lebron James')).toBeNull()
   })
 })
--- src/dataStructures/HashTable/index.js
+++ src/dataStructures/HashTable/index.js
@@ -1,52 +1,123 @@
+// the bucketing hash map
 class HashTable {
-  constructor(size) {
-    this.values = {}
-    this.numberOfValues = 0
-    this.size = size
+  constructor(size = 8) {
+    this._storage = []
+    this._count = 0
+    this._limit = size
   }
 
-  add(key, value) {
-    var hash = this.calculateHash(key)
-    if (!this.values.hasOwnProperty(hash)) {
-      this.values[hash] = {}
+  insert(key, value) {
+    //create an index for our storage location by passing it through our hashing function
+    const index = this.hashFunc(key, this._limit)
+    //retrieve the bucket at this particular index in our storage, if one exists
+    //[[ [k,v], [k,v], [k,v] ] , [ [k,v], [k,v] ]  [ [k,v] ] ]
+    var bucket = this._storage[index]
+    //does a bucket exist or do we get undefined when trying to retrieve said index?
+    if (!bucket) {
+      //create the bucket
+      var bucket = []
+      //insert the bucket into our hashTable
+      this._storage[index] = bucket
     }
-    if (!this.values[hash].hasOwnProperty(key)) {
-      this.numberOfValues++
+
+    var override = false
+    //now iterate through our bucket to see if there are any conflicting
+    //key value pairs within our bucket. If there are any, override them.
+    for (var i = 0; i < bucket.length; i++) {
+      var tuple = bucket[i]
+      if (tuple[0] === key) {
+        //overide value stored at this key
+        tuple[1] = value
+        override = true
+      }
     }
-    this.values[hash][key] = value
+
+    if (!override) {
+      //create a new tuple in our bucket
+      //note that this could either be the new empty bucket we created above
+      //or a bucket with other tupules with keys that are different than
+      //the key of the tuple we are inserting. These tupules are in the same
+      //bucket because their keys all equate to the same numeric index when
+      //passing through our hash function.
+      bucket.push([key, value])
+      this._count++
+      //now that we've added our new key/val pair to our storage
+      //let's check to see if we need to resize our storage
+      if (this._count > this._limit * 0.75) {
+        this.resize(this._limit * 2)
+      }
+    }
+    return this
   }
 
   remove(key) {
-    var hash = this.calculateHash(key)
-    if (this.values.hasOwnProperty(hash) && this.values[hash].hasOwnProperty(key)) {
-      delete this.values[hash][key]
-      this.numberOfValues--
-    }
-  }
-
-  calculateHash(key) {
-    return key.toString().length % this.size
-  }
-
-  search(key) {
-    var hash = this.calculateHash(key)
-    if (this.values.hasOwnProperty(hash) && this.values[hash].hasOwnProperty(key)) {
-      return this.values[hash][key]
-    } else {
+    var index = this.hashFunc(key, this._limit)
+    var bucket = this._storage[index]
+    if (!bucket) {
       return null
     }
-  }
-  length() {
-    return this.numberOfValues
-  }
-  print() {
-    var string = ''
-    for (var value in this.values) {
-      for (var key in this.values[value]) {
-        string += this.values[value][key] + ' '
+    //iterate over the bucket
+    for (var i = 0; i < bucket.length; i++) {
+      var tuple = bucket[i]
+      //check to see if key is inside bucket
+      if (tuple[0] === key) {
+        //if it is, get rid of this tuple
+        bucket.splice(i, 1)
+        this._count--
+        if (this._count < this._limit * 0.25) {
+          this._resize(this._limit / 2)
+        }
+        return tuple[1]
       }
     }
-    return string.trim()
+  }
+
+  retrieve(key) {
+    const index = this.hashFunc(key, this._limit)
+    const bucket = this._storage[index]
+
+    if (!bucket) {
+      return null
+    }
+
+    for (let i = 0; i < bucket.length; i++) {
+      var tuple = bucket[i]
+      if (tuple[0] === key) {
+        return tuple[1]
+      }
+    }
+
+    return null
+  }
+
+  hashFunc(str, max) {
+    return [...str].reduce((acc, letter) => {
+      acc = (acc << 5) + letter.charCodeAt(0)
+      acc = (acc & acc) % max
+      return acc
+    }, 0)
+  }
+
+  resize(newLimit) {
+    const oldStorage = this._storage
+
+    this._limit = newLimit
+    this._count = 0
+    this._storage = []
+
+    oldStorage.forEach(bucket => {
+      if (!bucket) {
+        return
+      }
+
+      bucket.forEach(tuple => {
+        this.insert(tuple[0], tuple[1])
+      })
+    })
+  }
+
+  retrieveAll() {
+    return this._storage
   }
 }
 
